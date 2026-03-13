0275d7e1d47aaa780e8b394a8e2a40836ab7cdcd
Minemario64 <charles.mitchell6425@outlook.com>
2026-03-13 18:05:50 -0400

    1.0.0 Hotfix 1: Made package use relative imports

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 import os
 
 if os.name == "nt":
-    from _win import Key, handleInput
+    from ._win import Key, handleInput
 
 elif os.name == "posix":
-    from _unix import Key, handleInput
+    from ._unix import Key, handleInput
 
 else:
     raise ImportError("Can't initialize. Only supports windows and posix systems")
